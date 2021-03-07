18c8897b7f68d05acf1f93fd9254deb35b61a5c0
Kittikorn <61011270001@msu.ac.th>
2021-03-07 14:18:52 +0700

    .

--- src/util/connectSCB.js
+++ src/util/connectSCB.js
@@ -1,7 +1,9 @@
 
 const SCB_API = 'https://fasteasy.scbeasy.com:8443';
+
+// DIW
 const API_REFRESH = 'ac0f0570-4ea1-4881-b2b9-7893f4f14516';
-const DEVICEID = '35f37d36-b091-483e-95d4-1c4c7bbbc9fc' //รหัสธาคาร ระบุตัวตน
+const DEVICEID = '697a3ce4-070e-4de9-9963-a9778fa14470' //รหัสธาคาร ระบุตัวตน
 
 // ข้อมูลบัญชี DIW
 const ACCOUNT_FROM = "4220708686" // กำนหด บัญชีผู้โอน (เรา)
