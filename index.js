917faffcaaaf5d4c83a7b398eede17430d05520d
Kittikorn <61011270001@msu.ac.th>
2021-03-15 11:28:02 +0700

    .

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const app = express();
 const USERID = require('./src/component/database/user');
 const UPDATE_BANK = require('./src/component/database/updateBank');
 const HOME = require('./src/component/home/welcome');
-const SCB_TANSACTION = require('./src/component/scb/transfer');
+
 const LINEBOT = require('./src/component/linebot/index');
 const BANK = require('./src/component/database/bank');
 const TRANSFER_DEPOSIT = require('./src/component/transfer/deposit');
 const TRANSFER_WITHDRAW = require('./src/component/transfer/withdraw');
+const TRANSFER_LOGIN = require('./src/component/scb/login');
 const SUPERLOT = require('./src/component/superlot/test');
 
 // UTIL
@@ -44,13 +45,13 @@ app.post(`${API_V1}/user/add`, USERID)
 app.post(`${API_V1}/user/update`, USERID)
 // BANK
 app.get(`${API_V1}/bank/all`, BANK)
-app.get(`${API_V1}/scb/login/refresh`, UPDATE_BANK)
 app.get(`${API_V1}/scb/transfer/eligiblebanks`, UPDATE_BANK)
 app.get(`${API_V1}/scb/update/eligiblebanks`, UPDATE_BANK)
 app.get(`${API_V1}/scb/add/eligiblebanks`, UPDATE_BANK)
 
 
 // Transfer
+app.post(`${API_V1}/scb/login/refresh`, TRANSFER_LOGIN)
 app.post(`${API_V1}/transfer/withdraw`, TRANSFER_WITHDRAW) 
 app.post(`${API_V1}/transfer/deposit`, TRANSFER_DEPOSIT)
 
