a5aa7ead8cc397bb7627dd801a4b395b3c47e58a
Kittikorn <61011270001@msu.ac.th>
2021-03-15 11:11:12 +0700

    update SCB config

--- src/util/connectSCB.js
+++ src/util/connectSCB.js
@@ -1,12 +1,15 @@
 
 const SCB_API = 'https://fasteasy.scbeasy.com:8443';
 
-// DIW
-const API_REFRESH = 'ac0f0570-4ea1-4881-b2b9-7893f4f14516';
+// POK
+const API_REFRESH = 'f2458590-6dd7-4b74-8f6f-a450630344a7';
 const DEVICEID = '697a3ce4-070e-4de9-9963-a9778fa14470' //รหัสธาคาร ระบุตัวตน
 
 // ข้อมูลบัญชี DIW
-const ACCOUNT_FROM = "4220708686" // กำนหด บัญชีผู้โอน (เรา)
+// const ACCOUNT_FROM = "4220708686" // กำนหด บัญชีผู้โอน (เรา)
+
+// ข้อมูลบัญชี POK
+const ACCOUNT_FROM = "8082733504" // กำนหด บัญชีผู้โอน (เรา)
 const ACCOUNT_FROMTYPE = "2" // กำหนด ประเภทบัญชีผู้โอน (เรา)
 
 module.exports = {
